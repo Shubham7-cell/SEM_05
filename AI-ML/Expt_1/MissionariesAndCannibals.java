a9ddd474cdc0d950d0919ccd894e6c20d7d5aba4
Shubham Golwal <76422167+Shubham7-cell@users.noreply.github.com>
2022-09-16 08:42:04 +0530

    finally at mid night

--- AI-ML/Expt_1/MissionariesAndCannibals.java
+++ AI-ML/Expt_1/MissionariesAndCannibals.java
@@ -5,12 +5,12 @@ public class MissionariesAndCannibals {
     public static boolean MissTwo(int[] list, String boat, boolean[] visited) {
         // These conditions check the bank status and then updates their location to
         // opposite bank
-        if (boat.equals("Left Bank") && visited[0] == false) {
+        if (boat.equals(" | Current Position: Left Bank") && visited[0] == false) {
             list[2] += 2;
             list[0] -= 2;
             visited[1] = true;
             return true;
-        } else if (boat.equals("Right Bank") && visited[1] == false) {
+        } else if (boat.equals(" | Current Position: Right Bank") && visited[1] == false) {
             list[2] -= 2;
             list[0] += 2;
             visited[0] = true;
@@ -24,12 +24,12 @@ public class MissionariesAndCannibals {
     public static boolean CanTwo(int[] list, String boat, boolean[] visited) {
         // These conditions check the bank status and then updates their location to
         // opposite bank
-        if (boat.equals("Left Bank") && visited[2] == false) {
+        if (boat.equals(" | Current Position: Left Bank") && visited[2] == false) {
             list[3] += 2;
             list[1] -= 2;
             visited[3] = true;
             return true;
-        } else if (boat.equals("Right Bank") && visited[3] == false) {
+        } else if (boat.equals(" | Current Position: Right Bank") && visited[3] == false) {
             list[3] -= 2;
             list[1] += 2;
             visited[2] = true;
@@ -43,12 +43,12 @@ public class MissionariesAndCannibals {
     public static boolean MissOne(int[] arr, String boat, boolean[] alrdyVisited) {
         // These conditions check the bank status and then updates their location to
         // opposite bank
-        if (boat.equals("Left Bank") && alrdyVisited[4] == false) {
+        if (boat.equals(" | Current Position: Left Bank") && alrdyVisited[4] == false) {
             arr[2] += 1;
             arr[0] -= 1;
             alrdyVisited[5] = true;
             return true;
-        } else if (boat.equals("Right Bank") && alrdyVisited[5] == false) {
+        } else if (boat.equals(" | Current Position: Right Bank") && alrdyVisited[5] == false) {
             arr[2] -= 1;
             arr[0] += 1;
             alrdyVisited[4] = true;
@@ -62,12 +62,12 @@ public class MissionariesAndCannibals {
     public static boolean CanOne(int[] arr, String boat, boolean[] visited) {
         // These conditions check the bank status and then updates their location to
         // opposite bank
-        if (boat.equals("Left Bank") && visited[6] == false) {
+        if (boat.equals(" | Current Position: Left Bank") && visited[6] == false) {
             arr[3] += 1;
             arr[1] -= 1;
             visited[7] = true;
             return true;
-        } else if (boat.equals("Right Bank") && visited[7] == false) {
+        } else if (boat.equals(" | Current Position: Right Bank") && visited[7] == false) {
             arr[3] -= 1;
             arr[1] += 1;
             visited[6] = true;
@@ -82,14 +82,14 @@ public class MissionariesAndCannibals {
     public static boolean MissOneCanOne(int[] arr, String boat, boolean[] visited) {
         // These conditions check the bank status and then updates their location to
         // opposite bank
-        if (boat.equals("Left Bank") && visited[8] == false) {
+        if (boat.equals(" | Current Position: Left Bank") && visited[8] == false) {
             arr[2] += 1;
             arr[0] -= 1;
             arr[3] += 1;
             arr[1] -= 1;
             visited[9] = true;
             return true;
-        } else if (boat.equals("Right Bank") && visited[9] == false) {
+        } else if (boat.equals(" | Current Position: Right Bank") && visited[9] == false) {
             arr[2] -= 1;
             arr[0] += 1;
             arr[3] -= 1;
@@ -113,100 +113,100 @@ public class MissionariesAndCannibals {
 
         // Condition for two missionaries on the boat via left bank
         // M should outnumber C / 0 M should be present at this bank
-        if ((list[0] - 2 > list[1] || list[0] - 2 == 0) && boat.equals("Left Bank")) {
+        if ((list[0] - 2 > list[1] || list[0] - 2 == 0) && boat.equals(" | Current Position: Left Bank")) {
             if (MissTwo(list, boat, visited)) {
                 System.out.println("Boat Status: (M M)\n");
-                boat = "Right Bank";
+                boat = " | Current Position: Right Bank";
                 MC(list, boat, visited);
             }
         }
 
         // Condition for two cannibals on the boat via left bank
         // C should be either 0 or greater than zero at this bank
-        if (list[1] - 2 >= 0 && boat.equals("Left Bank")) {
+        if (list[1] - 2 >= 0 && boat.equals(" | Current Position: Left Bank")) {
             if (CanTwo(list, boat, visited)) {
                 System.out.println("Boat Status: (C C)\n");
-                boat = "Right Bank";
+                boat = " | Current Position: Right Bank";
                 MC(list, boat, visited);
             }
         }
 
         // Condition for one missionary on the boat via left bank
         // M should be either 0 or greater than number of cannibals at this bank
-        if ((list[0] - 1 > list[1] || list[0] - 1 == 0) && boat.equals("Left Bank")) {
+        if ((list[0] - 1 > list[1] || list[0] - 1 == 0) && boat.equals(" | Current Position: Left Bank")) {
             if (MissOne(list, boat, visited)) {
                 System.out.println("Boat Status: (M _)\n");
-                boat = "Right Bank";
+                boat = " | Current Position: Right Bank";
                 MC(list, boat, visited);
             }
         }
 
         // Condition for one cannibal on the boat via left bank
         // C should be either 0 or greater than zero at this bank
-        if (list[1] - 1 >= 0 && boat.equals("Left Bank")) {
+        if (list[1] - 1 >= 0 && boat.equals(" | Current Position: Left Bank")) {
             if (CanOne(list, boat, visited)) {
                 System.out.println("Boat Status: (C _)\n");
-                boat = "Right Bank";
+                boat = " | Current Position: Right Bank";
                 MC(list, boat, visited);
             }
         }
 
         // Condition for one missionary and one cannibal on the boat via left bank
         // C and M should be equal in number at this bank
-        if (list[0] == list[1] && boat.equals("Left Bank")) {
+        if (list[0] == list[1] && boat.equals(" | Current Position: Left Bank")) {
             if (MissOneCanOne(list, boat, visited)) {
                 System.out.println("Boat Status: (M C)\n");
-                boat = "Right Bank";
+                boat = " | Current Position: Right Bank";
                 MC(list, boat, visited);
             }
         }
 
         // Condition for two missionaries on the boat via right bank
         // M should be greater than C or 0 at this bank
-        if ((list[2] - 2 > list[3] || list[2] - 2 == 0) && boat.equals("Right Bank")) {
+        if ((list[2] - 2 > list[3] || list[2] - 2 == 0) && boat.equals(" | Current Position: Right Bank")) {
             if (MissTwo(list, boat, visited)) {
                 System.out.println("Boat Status: (M M)\n");
-                boat = "Left Bank";
+                boat = " | Current Position: Left Bank";
                 MC(list, boat, visited);
             }
         }
 
         // Condition for two cannibals on the boat via right bank
         // C should be either 0 or greater than zero at this bank
-        if (list[3] - 2 >= 0 && boat.equals("Right Bank")) {
+        if (list[3] - 2 >= 0 && boat.equals(" | Current Position: Right Bank")) {
             if (CanTwo(list, boat, visited)) {
                 System.out.println("Boat Status: (C C)\n");
-                boat = "Left Bank";
+                boat = " | Current Position: Left Bank";
                 MC(list, boat, visited);
             }
         }
 
         // Condition for one missionary on the boat via right bank
         // M should be either 0 or greater than number of cannibals at this bank
-        if ((list[2] - 1 > list[3] || list[2] - 1 == 0) && boat.equals("Right Bank")) {
+        if ((list[2] - 1 > list[3] || list[2] - 1 == 0) && boat.equals(" | Current Position: Right Bank")) {
             if (MissOne(list, boat, visited)) {
                 System.out.println("Boat Status: (M _)\n");
-                boat = "Left Bank";
+                boat = " | Current Position: Left Bank";
                 MC(list, boat, visited);
             }
         }
 
         // Condition for one cannibal on the boat via right bank
         // C should be either 0 or greater than zero at this bank
-        if (list[3] - 1 >= 0 && boat.equals("Right Bank")) {
+        if (list[3] - 1 >= 0 && boat.equals(" | Current Position: Right Bank")) {
             if (CanOne(list, boat, visited)) {
                 System.out.println("Boat Status: (C _)\n");
-                boat = "Left Bank";
+                boat = " | Current Position: Left Bank";
                 MC(list, boat, visited);
             }
         }
 
         // Condition for one missionary and one cannibal on the boat via right bank
         // C and M should be equal in number at this bank
-        if (list[2] == list[3] && boat.equals("Right Bank")) {
+        if (list[2] == list[3] && boat.equals(" | Current Position: Right Bank")) {
             if (MissOneCanOne(list, boat, visited)) {
                 System.out.println(" (M C)\n");
-                boat = "Left Bank";
+                boat = " | Current Position: Left Bank";
                 MC(list, boat, visited);
             }
         }
@@ -214,15 +214,15 @@ public class MissionariesAndCannibals {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number Missionaries: ");
+        System.out.println("Enter the number Missionaries on the Left Bank: ");
         int missionaries = sc.nextInt();
-        System.out.print("Enter the number of Cannibals: ");
+        System.out.println("Enter the number of Cannibals on the Left Bank: ");
         int cannibals = sc.nextInt();
         System.out.println();
-
+        System.out.println("------------------Solution------------------\n");
         // Missionaries and Cannibals on left bank
         int[] arr = { missionaries, cannibals, 0, 0 };
-        String boat = "Left Bank";
+        String boat = " | Current Position: Left Bank";
         boolean[] visited = new boolean[10];
         MC(arr, boat, visited);
         sc.close();
